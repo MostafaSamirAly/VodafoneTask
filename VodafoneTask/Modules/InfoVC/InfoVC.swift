afcfb90fcec979a99b283bd2a1c6c8de7de0cb82
Mostafa Samir <61014478+MostafaSamirAly@users.noreply.github.com>
2021-07-27 22:55:51 +0200

    updated UI and project files management

--- VodafoneTask/Modules/InfoVC/InfoVC.swift
+++ VodafoneTask/Modules/InfoVC/InfoVC.swift
@@ -16,6 +16,7 @@ class InfoVC: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setupImage()
+        navigationItem.title = "Details Screen"
     }
     
     override func viewDidDisappear(_ animated: Bool) {
